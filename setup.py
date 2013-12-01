ac95201d6d4ab3e71984f1cebe73d4004195b434
weslleymberg <weslleym.lisboa@gmail.com>
2013-12-01 19:41:07 -0200

    Updates setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name = "specdog",
       version = "0.0.1",
       description = "Simple tool to aumomate test routines execution",
       author = "Weslleymberg Lisboa",
       author_email = "weslleym.lisboa@gmail.com",
-      url = "",
-      packages=["specdog",],
+      url = "http://github.com/weslleymberg/Specdog",
+      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       install_requires=["pyinotify",],
       entry_points="""
-            [console_scripts]
-            specdog = specdog:main
-            """,
+      [console_scripts]
+      specdog = specdog:main
+      """,
      )
