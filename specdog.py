fc3b26ef94efdd42c208359e7f6ec20959cef9f4
weslleymberg <weslleym.lisboa@gmail.com>
2013-12-01 18:15:37 -0200

    Improves feedback

--- specdog.py
+++ specdog.py
@@ -10,6 +10,7 @@ class OnProjectChangeHandler(pyinotify.ProcessEvent):
         self.command = command
 
     def _run_command(self):
+        sys.stderr.write("\x1b[2J\x1b[H")
         print >> sys.stdout, 'Running "%s"...' %(self.command)
         command_list = self.command.split(' ')
         subprocess.call(command_list)
@@ -19,11 +20,13 @@ class OnProjectChangeHandler(pyinotify.ProcessEvent):
             return
         self._run_command()
 
-def auto_command(path, extension, command):
+def run_specdog(path, extension, command):
     watch_manager = pyinotify.WatchManager()
     handler = OnProjectChangeHandler(path=path, extension=extension, command=command)
     pyinotify.AsyncNotifier(watch_manager, handler)
     watch_manager.add_watch(path, pyinotify.ALL_EVENTS, rec=True, auto_add=True)
+    sys.stderr.write("\x1b[2J\x1b[H")
+    print >> sys.stdout, "Monitoring current directory..."
     try:
         asyncore.loop()
     except KeyboardInterrupt:
@@ -40,4 +43,4 @@ if __name__ == '__main__':
     extension = ".py"
     command = sys.argv[1]
 
-    auto_command(path, extension, command)
+    run_specdog(path, extension, command)
